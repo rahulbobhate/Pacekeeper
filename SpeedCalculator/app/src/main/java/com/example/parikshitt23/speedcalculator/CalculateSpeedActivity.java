8b06609f43811612eda4d85765bba6b3f093ac36
Shashank Sharma <shashanks@ufl.edu>
2016-02-21 04:37:56 -0500

    Speed calculation & Demo activity

---
+++ SpeedCalculator/app/src/main/java/com/example/parikshitt23/speedcalculator/CalculateSpeedActivity.java
@@ -0,0 +1,135 @@
+package com.example.parikshitt23.speedcalculator;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class CalculateSpeedActivity extends Activity implements LocationListener{
+
+
+    TextView speedDisplay;
+    Button btn;
+
+    Location location1,location2;
+    long time1=0,time2=System.currentTimeMillis();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_speed);
+
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CalculateSpeedActivity.this, MusicActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)   != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        this.onLocationChanged(null);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calculate_speed, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.appDemo) {
+            Intent i = new Intent(CalculateSpeedActivity.this, DemoActivity.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if(time1==0)
+        {
+            time2 = System.currentTimeMillis()/1000;
+            location2 = location;
+            location1 = location;
+        }
+        else
+        {
+            location2 = location;
+            time2 = System.currentTimeMillis()/1000;
+        }
+
+        TextView txt = (TextView)this.findViewById(R.id.lat);
+        TextView txt1 = (TextView)this.findViewById(R.id.longitude);
+        TextView txt2 = (TextView)this.findViewById(R.id.speed);
+
+        if(location == null){
+            txt.setText("Lat -.-");
+            txt1.setText("Long -.-");
+            txt2.setText("Speed -.-");
+        }else{
+
+            float speed = location1.distanceTo(location2)/(time2-time1);
+            time1=System.currentTimeMillis()/1000;
+            location1 = location2;
+            txt.setText("Lat "+location.getLatitude());
+            txt1.setText("Long " +location.getLongitude());
+            txt2.setText("Speed " + speed);
+        }
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
